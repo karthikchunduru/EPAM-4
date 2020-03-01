cf776371a53021d903bb81057a4faf8e98d2f194
karthikchunduru <58825981+karthikchunduru@users.noreply.github.com>
2020-03-01 22:50:43 +0530

    Add files via upload

---
+++ Task4-CleanCode/README.md
@@ -0,0 +1,2 @@
+# epam-task4-logging
+ 
---
+++ Task4-CleanCode/src/main/java/epam/pep/task4CleanCode/Interest.java
@@ -0,0 +1,9 @@
+package epam.pep.task4CleanCode;
+
+public abstract class Interest {
+	public abstract double CalculateInterest(double principal, double rate, double time);
+}
+
+
+
+
